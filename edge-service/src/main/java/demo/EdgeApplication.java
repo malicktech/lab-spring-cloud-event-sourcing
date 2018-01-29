036dbbd323692cf9c27a51df4c75ecedf0f3b5f8
Malick DIOP <diop-malick@hotmail.fr>
2018-01-29 17:56:07 +0000

    API gateway service > Update dependencies

--- edge-service/src/main/java/demo/EdgeApplication.java
+++ edge-service/src/main/java/demo/EdgeApplication.java
@@ -1,31 +1,55 @@
 package demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.core.env.Environment;
+
+import demo.config.DefaultProfileUtil;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-@EnableResourceServer
+@EnableZuulServer
 @EnableHystrix
 public class EdgeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EdgeApplication.class, args);
-    }
+	private static final Logger log = LoggerFactory.getLogger(EdgeApplication.class);
 
-    @Configuration
-    public static class RestSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
+    private final Environment env;
+
+    public EdgeApplication(Environment env) {
+        this.env = env;
+    }
+    
+    public static void main(String[] args) throws UnknownHostException {        
+        SpringApplication app = new SpringApplication(EdgeApplication.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
         }
+        
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application '{}' is running! Access URLs:\n\t" +
+                "Local: \t\t{}://localhost:{}\n\t" +
+                "External: \t{}://{}:{}\n\t" +
+                "Profile(s): \t{}\n----------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            protocol,
+            env.getProperty("server.port"),
+            protocol,
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"),
+            env.getActiveProfiles());
     }
 }
