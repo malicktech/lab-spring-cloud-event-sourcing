8336c7d422f813eb52358ff202a18d031f716121
Malick DIOP <diop-malick@hotmail.fr>
2018-01-29 17:56:59 +0000

    Store service > Update dependencies

--- online-store-web/src/main/java/demo/OnlineStoreApplication.java
+++ online-store-web/src/main/java/demo/OnlineStoreApplication.java
@@ -1,32 +1,58 @@
 package demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import demo.config.DefaultProfileUtil;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-@EnableOAuth2Sso
 @EnableHystrix
-public class OnlineStoreApplication extends WebSecurityConfigurerAdapter {
+public class OnlineStoreApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OnlineStoreApplication.class, args);
+	
+	private static final Logger log = LoggerFactory.getLogger(OnlineStoreApplication.class);
+
+    private final Environment env;
+
+    public OnlineStoreApplication(Environment env) {
+        this.env = env;
     }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/index.html", "/login", "/", "/api/catalog/**",
-                        "/user", "/assets/**").permitAll()
-                .anyRequest().authenticated().and().csrf().disable();
+    
+    public static void main(String[] args) throws UnknownHostException {        
+        SpringApplication app = new SpringApplication(OnlineStoreApplication.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application '{}' is running! Access URLs:\n\t" +
+                "Local: \t\t{}://localhost:{}\n\t" +
+                "External: \t{}://{}:{}\n\t" +
+                "Profile(s): \t{}\n----------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            protocol,
+            env.getProperty("server.port"),
+            protocol,
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"),
+            env.getActiveProfiles());
     }
 
 }
