5a146d3b91d438b3eefbe5cfd7c9f3c3e988aa1e
Malick DIOP <diop-malick@hotmail.fr>
2018-01-29 17:55:49 +0000

    catalog service > Update dependencies

--- catalog-service/src/main/java/demo/CatalogServiceApplication.java
+++ catalog-service/src/main/java/demo/CatalogServiceApplication.java
@@ -1,21 +1,57 @@
 package demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
+
+import demo.config.DefaultProfileUtil;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
 public class CatalogServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CatalogServiceApplication.class, args);
-    }
+	
+	private static final Logger log = LoggerFactory.getLogger(CatalogServiceApplication.class);
 
+    private final Environment env;
+
+    public CatalogServiceApplication(Environment env) {
+        this.env = env;
+    }
+    
+    public static void main(String[] args) throws UnknownHostException {        
+        SpringApplication app = new SpringApplication(CatalogServiceApplication.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application '{}' is running! Access URLs:\n\t" +
+                "Local: \t\t{}://localhost:{}\n\t" +
+                "External: \t{}://{}:{}\n\t" +
+                "Profile(s): \t{}\n----------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            protocol,
+            env.getProperty("server.port"),
+            protocol,
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"),
+            env.getActiveProfiles());
+    }
+    
     @LoadBalanced
     @Bean
     public RestTemplate loadRestTemplate() {
---
+++ catalog-service/src/main/java/demo/config/Constants.java
@@ -0,0 +1,10 @@
+package demo.config;
+
+public interface Constants {
+	
+	// Spring profiles for development, test and production
+    String SPRING_PROFILE_DEVELOPMENT = "dev";
+    String SPRING_PROFILE_TEST = "test";
+    String SPRING_PROFILE_PRODUCTION = "prod";
+
+}
